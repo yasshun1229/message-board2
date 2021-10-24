480227191b66cd240eb657d9b9a30de1bb25a2ed
shunsuke.yasuda <69839892+yasshun1229@users.noreply.github.com>
2021-10-24 06:59:23 +0000

    add db-seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# テストデータの生成
+
+# レコードを3件表示する
+# Message.create(title: "test title 1 ", content: "test content1")
+# Message.create(title: "test title 2 ", content: "test content2")
+# Message.create(title: "test title 3 ", content: "test content3")
+
+# レコードを100件表示する
+(1..100).each do |number|
+  Message.create(title: "test title" + number.to_s, content: "test content" + number.to_s )
+end
+
+# rails db:seed
